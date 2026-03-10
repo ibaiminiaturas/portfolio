24bce7eba70cdcf4f0d2206b53a8644a0ba2a31d
Ibai Ramirez <ibaiminiaturas@gmail.com>
2026-03-10 22:24:32 +0100

    colroes

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
 safelist: [
   "hidden","block","absolute","relative",
   "bg-gray-800","bg-gray-700","bg-gray-600",
+  "text-gray-700","text-gray-500","text-gray-300",
   "text-white","mt-2","rounded","w-max",
   'hover:bg-gray-500',
   'hover:text-gray-200',
