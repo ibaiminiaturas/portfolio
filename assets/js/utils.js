a0ae64d3df665a31afd2cd2f6836f48ffceae6df
IBERNYX\iramirez <ibai@bdpcenter.com>
2026-03-11 11:03:50 +0100

    footer y header bien creo

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,8 +1,14 @@
 // assets/js/utils.js
 function loadHeader(extra) {
-      const base = location.hostname.includes("github.io") ? "/portfolio/" : "" + extra;
+      const base = (location.hostname.includes("github.io") ? "/portfolio/" : "") + extra;
       document.getElementById('header-logo').src = `${base}assets/images/ibai-miniaturas-circle.png`;
       document.getElementById('flag-es').src = `${base}assets/images/flag-es.svg`;
       document.getElementById('flag-uk').src = `${base}assets/images/flag-uk.png`;
       document.getElementById('ig-icon').src = `${base}assets/images/instagram.png`;
+    }
+
+    function loadFooter(extra) {
+      const base = (location.hostname.includes("github.io") ? "/portfolio/" : "") + extra;
+      document.getElementById('footer-logo').src = `${base}assets/images/ibai-miniaturas-circle.png`;
+      document.getElementById('footer-ig-icon').src = `${base}assets/images/instagram.png`;
     }
