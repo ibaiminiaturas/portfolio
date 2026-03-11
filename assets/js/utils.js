7992591199d3406cdc7eb7a9f725d4c7eac17c3b
IBERNYX\iramirez <ibai@bdpcenter.com>
2026-03-11 11:16:21 +0100

    error tonto

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -2,6 +2,7 @@
 function loadHeader(extra) {
     console.log("leadheader called");
       const base = (location.hostname.includes("github.io") ? "portfolio/" : "") + extra;
+      console.log(base);
       document.getElementById('header-logo').src = `${base}assets/images/ibai-miniaturas-circle.png`;
       document.getElementById('flag-es').src = `${base}assets/images/flag-es.svg`;
       document.getElementById('flag-uk').src = `${base}assets/images/flag-uk.png`;
@@ -10,7 +11,9 @@ function loadHeader(extra) {
 
     function loadFooter(extra) {
         console.log("loadFooter called");
+
       const base = (location.hostname.includes("github.io") ? "portfolio/" : "") + extra;
+              console.log(base);
       document.getElementById('footer-logo').src = `${base}assets/images/ibai-miniaturas-circle.png`;
       document.getElementById('footer-ig-icon').src = `${base}assets/images/instagram.png`;
     }
