012f06848369fa3f06587145408c369b600a0de1
IBERNYX\iramirez <ibai@bdpcenter.com>
2026-03-11 11:13:41 +0100

    footer mal en pages

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 // assets/js/config.js
 // Define BASE_PATH dinámicamente
 const BASE_PATH = (function() {
+    console.log(location.hostname);
   const isGithub = location.hostname.includes("github.io");
   if (isGithub) {
     // Ajusta 'portfolio' al nombre de tu repo en GitHub
--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,6 +1,7 @@
 // assets/js/utils.js
 function loadHeader(extra) {
-      const base = (location.hostname.includes("github.io") ? "/portfolio/" : "") + extra;
+    console.log("leadheader called");
+      const base = (location.hostname.includes("github.io") ? "portfolio/" : "") + extra;
       document.getElementById('header-logo').src = `${base}assets/images/ibai-miniaturas-circle.png`;
       document.getElementById('flag-es').src = `${base}assets/images/flag-es.svg`;
       document.getElementById('flag-uk').src = `${base}assets/images/flag-uk.png`;
@@ -8,7 +9,8 @@ function loadHeader(extra) {
     }
 
     function loadFooter(extra) {
-      const base = (location.hostname.includes("github.io") ? "/portfolio/" : "") + extra;
+        console.log("loadFooter called");
+      const base = (location.hostname.includes("github.io") ? "portfolio/" : "") + extra;
       document.getElementById('footer-logo').src = `${base}assets/images/ibai-miniaturas-circle.png`;
       document.getElementById('footer-ig-icon').src = `${base}assets/images/instagram.png`;
     }
