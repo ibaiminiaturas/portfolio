8618a606bf58e14b22ca8affc4697719980cc58e
Ibai Ramirez <ibaiminiaturas@gmail.com>
2026-03-15 16:17:38 +0100

    logos en movil

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -2,8 +2,18 @@
 function loadHeader(extra) {
     
       const base = (location.hostname.includes("github.io") ? "/portfolio/" : "") + extra;
-      document.getElementById('header-logo').src = `${base}assets/images/ibai-miniaturas-circle.webp`;
-      document.getElementById('flag-es').src = `${base}assets/images/flag-es.svg`;
-      document.getElementById('flag-uk').src = `${base}assets/images/flag-uk.png`;
+    const flagEs = document.getElementById('flag-es');
+    const flagEn = document.getElementById('flag-en');
+    const logo = document.getElementById('header-logo');
+    const logo_mobile = document.getElementById('header-logo-mobile');
+
+    // Usamos el "Optional Chaining" o un simple IF
+    if (flagEs) flagEs.src = `${BASE_PATH}/assets/images/flag-es.svg`;
+    if (flagEn) flagEn.src = `${BASE_PATH}/assets/images/flag-en.png`;
+    if (logo)   logo.src   = `${BASE_PATH}/assets/images/ibai-miniaturas-circle.webp`;
+if(logo_mobile) logo_mobile.src =  `${BASE_PATH}/assets/images/ibai-miniaturas-circle.webp`;
       document.getElementById('ig-icon').src = `${base}assets/images/instagram.png`;
+      // document.getElementById('flag-es-mobile').src = `${base}assets/images/flag-es.svg`;
+      // document.getElementById('flag-uk-mobile').src = `${base}assets/images/flag-uk.png`;
+      // document.getElementById('ig-icon-mobile').src = `${base}assets/images/instagram.png`;
     }
